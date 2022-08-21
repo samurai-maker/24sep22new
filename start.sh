3aeecfa5cdd035c7f279d80949ada8a42478bdb4
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-08-21 08:06:03 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/samurai-maker/EvaMaria.git /EvaMaria
+  git clone https://github.com/samurai-maker/neeewgroupfilta30.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
